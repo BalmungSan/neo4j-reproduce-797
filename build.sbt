0303a590af056eb8a3f388c22724a338f6a69ebc
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2020-12-06 11:17:53 -0500

    Removing Akka

--- build.sbt
+++ build.sbt
@@ -3,11 +3,9 @@ scalaVersion := "2.13.4"
 organization := "neotypes"
 
 val neo4jDriverVersion = "4.2.0"
-val akkaStreamVersion = "2.6.10"
 val logbackVersion = "1.2.3"
 
 libraryDependencies ++= Seq(
   "org.neo4j.driver" % "neo4j-java-driver" % neo4jDriverVersion,
-  "com.typesafe.akka" %% "akka-stream" % akkaStreamVersion,
   "ch.qos.logback" % "logback-classic" % logbackVersion
 )
--- src/main/scala/neotypes/Main.scala
+++ src/main/scala/neotypes/Main.scala
@@ -1,14 +1,10 @@
 package neotypes
 
-import akka.NotUsed
-import akka.actor.ActorSystem
-import akka.stream.Materializer
-import akka.stream.scaladsl.{Sink, Source}
 import org.neo4j.{driver => neo4j}
-import org.reactivestreams.Publisher
+import org.reactivestreams.{Publisher, Subscriber, Subscription}
 
 import java.util.concurrent.Executors
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
 import scala.concurrent.duration.Duration
 import scala.jdk.CollectionConverters._
 import scala.util.control.NoStackTrace
@@ -17,12 +13,6 @@ object Main {
   implicit val ec =
     ExecutionContext.fromExecutorService(
       Executors.newSingleThreadExecutor()
-    )
-
-  implicit val system =
-    ActorSystem(
-      name = "QuickStart",
-      defaultExecutionContext = Some(ec)
     )
 
   def main(args: Array[String]): Unit = {
@@ -68,7 +58,7 @@ object Main {
         ex.printStackTrace()
     }
 
-    Await.ready(app.flatMap(_ => system.terminate()), Duration.Inf)
+    Await.ready(app, Duration.Inf)
     println()
     println("-------------------------------------------------")
     println(s"Final metrics: ${driver.metrics.connectionPoolMetrics.asScala}")
@@ -78,7 +68,7 @@ object Main {
 }
 
 final class NeotypesSession (session: neo4j.reactive.RxSession)
-                            (implicit ec: ExecutionContext, mat: Materializer) {
+                            (implicit ec: ExecutionContext) {
   import Syntax._
 
   def run(query: String): Future[Option[Map[String, String]]] = {
@@ -86,39 +76,58 @@ final class NeotypesSession (session: neo4j.reactive.RxSession)
       tx
         .run(query)
         .records
-        .toStream
-        .map { record =>
-          record
-            .fields
-            .asScala
-            .iterator
-            .map(p => p.key -> p.value.toString)
-            .toMap
-        }.single
+        .toFuture
+        .map { recordOption =>
+          recordOption.map { record =>
+            record
+              .fields
+              .asScala
+              .iterator
+              .map(p => p.key -> p.value.toString)
+              .toMap
+          }
+        }
 
     for {
-      tx <- session.beginTransaction.toStream.single.transform(_.flatMap(_.toRight(left = NoTransactionError).toTry))
+      tx <- session.beginTransaction.toFuture.transform(_.flatMap(_.toRight(left = NoTransactionError).toTry))
       result <- runQuery(tx)
-      _ <- tx.commit[Unit].toStream.void
+      _ <- tx.commit[Unit].toFuture
     } yield result
   }
 }
 
 object Syntax {
   implicit final class PublisherOps[A] (private val publisher: Publisher[A]) extends AnyVal {
-    def toStream: Source[A, NotUsed] =
-      Source.fromPublisher(publisher)
-  }
+    def toFuture(implicit ec: ExecutionContext): Future[Option[A]] = {
+      val promise = Promise[Option[A]]()
 
-  implicit final class StreamOps[A] (private val sa: Source[A, NotUsed]) extends AnyVal {
-    def list(implicit mat: Materializer): Future[Seq[A]] =
-      sa.runWith(Sink.seq)
+      val subscriber = new Subscriber[A] {
+        var s: Subscription = _
 
-    def single(implicit mat: Materializer): Future[Option[A]] =
-      sa.take(1).runWith(Sink.lastOption)
+        override def onSubscribe(subscription: Subscription): Unit = {
+          s = subscription
+          Future(s.request(1))
+        }
 
-    def void(implicit mat: Materializer, ec: ExecutionContext): Future[Unit] =
-      sa.runWith(Sink.ignore).map(_ => ())
+        override def onNext(a: A): Unit = {
+          promise.success(Some(a))
+          Future(s.cancel())
+        }
+
+        override def onError(ex: Throwable): Unit = {
+          promise.failure(ex)
+        }
+
+        override def onComplete(): Unit = {
+          if (!promise.isCompleted) {
+            promise.success(None)
+          }
+        }
+      }
+      publisher.subscribe(subscriber)
+
+      promise.future
+    }
   }
 }
 
